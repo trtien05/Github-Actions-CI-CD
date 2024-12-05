0fd1d4e8f96a156dd994702b510220955e5311f7
Ho Tran Tien <trantien100700@gmail.com>
2024-12-05 17:27:41 +0700

    fix text

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,12 +5,9 @@ type Params = {
 }
 
 export default function Page({ params }: Params) {
-<<<<<<< HEAD
-  return <><h1>Slug: {params.slug}</h1> <p>Tien</p> <p>Welcome to HCM</p></>;
-=======
   return <>
   <h1>Slug: {params.slug}</h1>
   <p>Tien</p>
+  <p>Welcome to HCM</p>
   </>
->>>>>>> 23e66dde5d5952b759c851c06c2c5b52830df847
 }
