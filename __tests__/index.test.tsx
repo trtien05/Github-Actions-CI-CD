226de84fb3490cad7f722dff33f7c995d66a2b55
Ho Tran Tien <trantien100700@gmail.com>
2024-12-05 18:12:31 +0700

    fix title name

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -9,7 +9,7 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to holetex/i,
+      name: /welcome to Ho Tran Tien/i,
     })
 
     expect(heading).toBeInTheDocument()
--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -10,7 +10,7 @@ it('App Router: Works with dynamic route segments', () => {
   expect(screen.getByRole('heading')).toHaveTextContent('Slug: Test')
 })
 
-it('HoleTex should be in the document', () => {
+it('Tien should be in the document', () => {
   render(<Page params={{ slug: 'Test' }} />);
   expect(screen.getByText('Tien')).toBeInTheDocument();
 })
